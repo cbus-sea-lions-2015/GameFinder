f3138e6c06c80800df8dd73337ddb6e370958c56
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-16 16:55:32 -0400

    created game model

---
+++ app/models/game.rb
@@ -0,0 +1,12 @@
+class Game < ActiveRecord::Base
+ has_many :games_categories
+ has_many :categories, through: :games_categories
+
+ has_many :games_mechanics
+ has_many :mechanics, through: :games_mechanics
+
+ has_many :games_libraries
+ has_many :libraries, through: :games_libraries
+
+ validates :bgg_id, uniqueness: :true
+end
