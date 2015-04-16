601bf4646db3a83160d8bd4ebf14ef14a6d52f6b
Daniel Martinez <mikedmtz@gmail.com>
2015-04-16 16:10:11 -0400

    add user model, user controller, and user feature tests

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature "User visits page without login in" do
+  before :each do
+    visit '/logout'
+  end
+
+  scenario "when a user visits the home page the user can see brand and links" do
+    visit '/'
+    expect(page).to have_content("GameFinder")
+    expect(page).to have_link("Log In")
+    expect(page).to have_link("Sign Up")
+  end
+
+end
