2e1ddc05321b1b76f8e15cb92d03e51595594dfa
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-18 14:22:06 -0400

    reduce api calls to new games only

--- app/workers/games_fetcher.rb
+++ app/workers/games_fetcher.rb
@@ -4,6 +4,6 @@ class GamesFetcher
   def perform(game_ids, library_id)
     game_json = BggApi.new.thing({ id: game_ids.join(",") })
 
-    BggParser.parse(game_json, library_id)
+    BggParser.games_parse(game_json, library_id)
   end
 end
--- app/workers/libraries_fetcher.rb
+++ app/workers/libraries_fetcher.rb
@@ -2,7 +2,7 @@ class LibrariesFetcher
   include Sidekiq::Worker
 
   def perform(game_ids, library_id)
-    game_ids.each_slice(5) do |slice|
+    game_ids.each_slice(100) do |slice|
       GamesFetcher.perform_async(slice, library_id)
     end
   end
