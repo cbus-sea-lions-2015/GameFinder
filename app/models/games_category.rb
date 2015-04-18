d1814dd23f69317ee8c9aa2dc9570b1b5f84e830
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-18 10:32:40 -0400

    enforce game_id not nil on join table models

--- app/models/games_category.rb
+++ app/models/games_category.rb
@@ -1,4 +1,7 @@
 class GamesCategory < ActiveRecord::Base
   belongs_to :game
   belongs_to :category
+
+  validates :game_id, presence: true
+  validates :category_id, presence: true
 end
--- app/workers/users_fetcher.rb
+++ app/workers/users_fetcher.rb
@@ -2,11 +2,11 @@ class UsersFetcher
   include Sidekiq::Worker
 
   def perform(bgg_username)
-    library = Library.find_or_create_by(bgg_username)
+    library = Library.find_or_create_by(bgg_username: bgg_username)
     bgg_user_library = BggApi.new.collection({ username: bgg_username })
 
     collection_array = bgg_user_library["item"].map { |game| game["objectid"].to_i }
 
-    LibrariesFetcher(collection_array, library.id)
+    LibrariesFetcher.perform_async(collection_array, library.id)
   end
 end
