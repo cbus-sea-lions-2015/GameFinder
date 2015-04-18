50f56a107460e72b60b669dbfc2f607822955fdf
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-17 22:17:26 -0400

    library now has bgg_username; many users

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,7 +2,7 @@ class Library < ActiveRecord::Base
   has_many :games_libraries
   has_many :games, through: :games_libraries
 
-  belongs_to :user, inverse_of: :library
+  has_many :users
 
-  validates :user, presence: true
+  validates :bgg_username, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  has_one :library, inverse_of: :user
 
   validates :email, presence: true
   validates :email, :uniqueness => true, :format => { :with => /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, :message => "Invalid email format." }
   validates :password_digest, presence: true
+
+  belongs_to :library, inverse_of: :user
 
   has_secure_password
 end
