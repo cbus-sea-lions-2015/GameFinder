27d358efed2b212b829d8a753bded2ff7c5eca5e
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-20 11:29:00 -0400

    fix users migration

--- db/migrate/20150420144754_nuke_users_table.rb
+++ db/migrate/20150420144754_nuke_users_table.rb
@@ -1,7 +1,8 @@
 class NukeUsersTable < ActiveRecord::Migration
-  def change_table :users do |t|
-    t.remove :email, :password_digest
-    t.string :auth0_id, null: false
-    t.string :email
+  def change
+    remove_column :users, :email
+    remove_column :users, :password_digest
+    add_column :users, :auth0_id, :string
+    add_column :users, :email, :string
   end
 end
