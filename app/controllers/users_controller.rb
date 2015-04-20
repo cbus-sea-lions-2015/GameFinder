6f830be63d9773e64204a3d43c2e0901c1cfbab7
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-20 19:32:39 -0400

    new users created, tests nuked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate, only: [:index, :new, :create]
 
   def index
-    # users = User.all
-    # render json: users, status: 200
+    users = User.all
+    render json: users, status: 200
   end
 
   def new
@@ -11,20 +11,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: {'token' => @user.auth_token}.to_json
-    end
+    user = User.find_or_create_by(auth0_id: params["token"])
+    render json: user.to_json
   end
 
   def show
     render json: @user.to_json
   end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
-    end
 
 end
